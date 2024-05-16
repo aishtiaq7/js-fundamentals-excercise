95da5440fd7e6f961f11b87541414082cdc697cf
aishtiaq <awshaf_ishtiaque@sfu.ca>
2024-05-16 01:16:48 -0700

    ex 1 on rl in scratch.js

--- exercises/scratch.js
+++ exercises/scratch.js
@@ -1,17 +1,25 @@
-
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-rl.question('Input name: ', (name) => {// Ask the first question
-  console.log(`given -> ${name}!`, `which is of type ${typeof name}`);
+// Ask the first question
+rl.question("Input name: ", (name) => {
+  //   console.log(`given -> ${name}!`, `which is of type ${typeof name}`);
 
+  //   rl.question("Input 2nd param: ", (param) => {
+  //     console.log(`given -> ${param}!`, `which is also of type ${typeof param}`);
+  //     rl.close();
+  //   });
 
+  const number = parseFloat(name); // Change your number to test your if statement
+  console.log("number:", typeof number);
+  console.log("number:", number);
 
-  rl.question('Input 2nd param: ', (param) => { //2nd question..
-    console.log(`given -> ${param}!`, `which is also of type ${typeof param}`);
-    rl.close(); //close readline interface
-  });
+    if (number % 2 === 0) {
+      console.log("even");
+    } else {
+      console.log("odd");
+    }
 });
